47034fd72c7be1083b27ca68b965b01697926121
EdoPolatic <edo@polatic.co.id>
2014-04-15 17:42:26 +0700

    Initial Commit

---
+++ Polatic/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  Polatic
+//
+//  Created by polatic on 4/15/14.
+//  Copyright (c) 2014 polatic. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
---
+++ Polatic/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  Polatic
+//
+//  Created by polatic on 4/15/14.
+//  Copyright (c) 2014 polatic. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
